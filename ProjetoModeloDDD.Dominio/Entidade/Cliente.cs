46dec1cb506cf8720d35504c7834096a6bd7d899
Gilmar Borges Silva <jiraspiom@yahoo.com.br>
2018-10-13 15:56:38 -0300

    Adicionado classe cliente, com alguma propriedades, e uma pequena regra de negocio para seguir exemplo

---
+++ ProjetoModeloDDD.Dominio/Entidade/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjetoModeloDDD.Dominio.Entidade
+{
+    class Cliente
+    {
+        public int ClienteID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public bool ClienteExpecial(Cliente cliente)
+        {
+            //verifica se cliente ta ativo, e que tenha mais de 5 anos de cadastro
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+
+    }
+}
