169065c2dc58f021c2388a6917e833777f0660c1
Gilmar Borges Silva <jiraspiom@yahoo.com.br>
2018-10-13 16:04:33 -0300

    Classe ProjetoModeloContexto erda de BdContext e foi intalado a referencia Install-Package EntityFramework via package Manager Console, no projetoModelo.Dados

--- ProjetoModeloDDD.Infraestrutura.Dados/Contexto/ProjetoModeloContexto.cs
+++ ProjetoModeloDDD.Infraestrutura.Dados/Contexto/ProjetoModeloContexto.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ProjetoModeloDDD.Infraestrutura.Dados.Contexto
 {
-    class ProjetoModeloContexto
+    class ProjetoModeloContexto : BdContext
     {
+
     }
 }
